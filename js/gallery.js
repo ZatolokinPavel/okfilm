5b0ead186f3f04f316ecaae57d5f9b2754186e1e
Zatolokin Pavel <ZatolokinPavel@yandex.ru>
2016-04-19 18:42:31 +0300

    try to add lightbox

--- js/gallery.js
+++ js/gallery.js
@@ -4,16 +4,28 @@
  * Created by Павел on 18.04.2016.
  */
 
+
+// Навешиваем обработчики клика на все фотки всех галерей на странице.
 $(function() {
     var allGallerys = document.getElementsByClassName('gallery');
     for (var i=0; i < allGallerys.length; i++) {
-        var gallery = $(allGallerys[i]);
-        gallery.children().click(function(link) {showOnePhoto(link);});
+        allGallerys[i].onclick = function(e) {
+            var target = e.target;
+            if(target.nodeName == 'IMG') {target = target.parentNode;}      // если нажатие пришлось не на ссылку а на вложенную в неё картинку, то получаем ссылку
+            // Обрабатываем все клики кроме клика колёсиком мыши.
+            // Потому что клик колёсиком должен просто открыть фотку в новой вкладке.
+            if (e.which != 2 && e.button != 1 && target.nodeName == 'A') {
+                showOnePhoto(target);
+                return false;
+            }
+        }
     }
 });
 
-function showOnePhoto(link) {
 
+// По клику на фотке открываем её в модальном окне
+function showOnePhoto(link) {
     console.log(link);
-    return false;
+    document.getElementById('lightbox-wrap').style.display = "block";
+    document.getElementById('lightbox-overlay').style.display = "block";
 }
