b0a4f87b44c2fc33563236f3490291a0ef9b77a1
ZatolokinPavel <ZatolokinPavel@yandex.ru>
2016-04-23 02:44:53 +0300

    show images on gallery by js

--- js/gallery.js
+++ js/gallery.js
@@ -7,6 +7,7 @@
 
 // Навешиваем обработчики клика на все фотки всех галерей на странице.
 $(function() {
+    document.getElementById('lightbox_overlay').addEventListener('click', closeLightBox);
     var allGallerys = document.getElementsByClassName('gallery');
     for (var i=0; i < allGallerys.length; i++) {
         allGallerys[i].onclick = function(e) {
@@ -25,7 +26,55 @@ $(function() {
 
 // По клику на фотке открываем её в модальном окне
 function showOnePhoto(link) {
-    console.log(link);
-    document.getElementById('lightbox-wrap').style.display = "block";
-    document.getElementById('lightbox-overlay').style.display = "block";
+    var winWidth  = window.innerWidth;          // ширина видимой области экрана
+    var winHeight = window.innerHeight;         // высота видимой области экрана
+    var scrollTop = $(window).scrollTop();      // верхняя часть видимой области окна
+    var scrollLeft = $(window).scrollLeft();    // левая часть видимой области окна
+
+    var img  = new Image;
+    var wrap = document.getElementById('lightbox_wrap');
+    var content = document.getElementById('lightbox_content');
+    var overlay = document.getElementById('lightbox_overlay');
+
+    img.src = link.href;
+    img.alt = link.title;
+    img.id  = 'lightbox_img';
+    img.onload = function() {
+        var imgW = img.naturalWidth;
+        var imgH = img.naturalHeight;
+        var ratio = img.naturalWidth / img.naturalHeight;
+        var H = winHeight * ratio * 0.9 <= winWidth ? winHeight * 0.9 : winWidth * 0.9 / ratio;
+        var W = (H - 20) * ratio + 20;
+        wrap.style.width  = W + 'px';
+        wrap.style.height = H + 'px';
+        wrap.style.top = (scrollTop + winHeight*0.05) + 'px';
+        wrap.style.left = (scrollLeft + (winWidth - W)/2) + 'px';
+        content.appendChild(img);
+        overlay.style.display = "block";
+        wrap.style.display = "block";
+    };
+
+    //link = $(link);
+    //var nextLink = link.next('a')[0];
+    //var prevLink = link.prev('a')[0];
+    //
+    //wrap.addEventListener('wheel', function(e){
+    //    e = e || window.event;
+    //    var delta = e.deltaY || e.detail || e.wheelDelta;
+    //    if (delta > 0 && nextLink) { console.log(nextLink); }
+    //    else if (prevLink) { console.log(prevLink); }
+    //    e.preventDefault ? e.preventDefault() : (e.returnValue = false);
+    //});
+}
+
+//function changePhoto(link) {
+//    document.getElementById('lightbox_content').innerHTML = "";
+//    showOnePhoto(link);
+//}
+
+// Закрытие модального окна с фотографией
+function closeLightBox() {
+    document.getElementById('lightbox_wrap').style.display = "none";
+    document.getElementById('lightbox_overlay').style.display = "none";
+    document.getElementById('lightbox_content').innerHTML = "";
 }
