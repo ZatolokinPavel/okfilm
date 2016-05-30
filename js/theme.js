085cc3d21901f950cc7ba8d73a7399f1eb01c449
ZatolokinPavel <ZatolokinPavel@yandex.ru>
2016-05-30 22:24:02 +0300

    remake background parallax script

--- js/theme.js
+++ js/theme.js
@@ -9,28 +9,25 @@
 var timerObj = {};
 
 
-// Parallax Scrolling Speed.
-// Set the scrolling speed of the background parallax image.
-// Рекомендуется устанавливать значения 1.5, 3 или 6
-var parallax_ratio = 2;
-
 // Встроеный API для запуска любых типов анимации в браузере.
-var requestAnimationFrame = window.requestAnimationFrame
-                         || window.webkitRequestAnimationFrame
-                         || window.mozRequestAnimationFrame
-                         || window.msRequestAnimationFrame
-                         || window.oRequestAnimationFrame
-                         || function(callback){setTimeout(callback,1e3/60)};
+//var requestAnimationFrame = window.requestAnimationFrame
+//                         || window.webkitRequestAnimationFrame
+//                         || window.mozRequestAnimationFrame
+//                         || window.msRequestAnimationFrame
+//                         || window.oRequestAnimationFrame
+//                         || function(callback){setTimeout(callback,1e3/60)};
 
 
 // Parallax Background Scrolling
 (function backgroundParallax() {
     var container = document.getElementById('parallaxBackground');
-    (function animloop(){
+    // Parallax Scrolling Speed of the background image.
+    // Рекомендуется устанавливать значения 1.5, 3 или 6
+    var parallax_ratio = 2;
+    window.addEventListener('scroll', function() {          // вешаем на скролл прокрутку фона
         var x = window.pageYOffset / parallax_ratio;
         container.style.top =  x+'px';
-        requestAnimationFrame(animloop);
-    }());
+    });
 }());
 
 
