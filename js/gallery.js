99a11ab9f8394138b5b570ff20decd7fe7d0c92c
ZatolokinPavel <ZatolokinPavel@yandex.ru>
2017-06-23 22:16:30 +0300

    remake gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -23,7 +23,9 @@ var GallerySliderOKFilm = function() {
         $(this).parent().parent().children().width('');
     };
 
-    $('.gallery_slider a').mouseover(selectSliderElement).mouseleave(deselectSliderElement);
+    $('.gallery_slider a')
+        .mouseover(selectSliderElement)
+        .mouseleave(deselectSliderElement);
 };
 $(GallerySliderOKFilm);
 
@@ -31,19 +33,39 @@ $(GallerySliderOKFilm);
 
 
 
+// Показ фотографий в модальном окне на весь экран
 var GalleryOKFilm = function() {
-    var lightbox,
-        wrap,
-        content,
-        overlay,
-        nextLink,
-        prevLink;
+    var lightbox,                   // весь блок с модальным окном отображения картинок
+        wrap,                       // обёртка, в которой находятся фотография и элементы управления
+        content,                    // блок с фотографией
+        overlay,                    // подложка, затеняющая остальную часть сайта
+        $arrow_l,                   // ссылка на предыдущее фото
+        $arrow_r,                   // ссылка на следующее фото
+        nextLink = null,            // предыдущее фото
+        prevLink = null;            // следующее фото
 
-    var init = function() {
-        createModalWindow();
+    var _init = function() {
+        var allGallerys = document.getElementsByClassName('gallery');
+        if(allGallerys.length > 0) {
+            _createModalWindow();
+            _addEventListeners();
+        }
+        for (var i=0; i < allGallerys.length; i++) {
+            allGallerys[i].onclick = function(e) {
+                var target = e.target;
+                if(target.nodeName == 'IMG') {target = target.parentNode;}      // если нажатие пришлось не на ссылку а на вложенную в неё картинку, то получаем ссылку
+                // Обрабатываем все клики кроме клика колёсиком мыши.
+                // Потому что клик колёсиком должен просто открыть фотку в новой вкладке.
+                if (e.which != 2 && e.button != 1 && target.nodeName == 'A') {
+                    _showOnePhoto(target);
+                    return false;
+                }
+            }
+        }
     };
 
-    var createModalWindow = function() {
+    // Создание элементов модального окна
+    var _createModalWindow = function() {
         lightbox = document.createElement('div');
         lightbox.style.display = 'none';
         document.body.appendChild(lightbox);
@@ -53,129 +75,110 @@ var GalleryOKFilm = function() {
             '<div id="lightbox_wrap">' +
                 '<div id="lightbox_content"></div>' +
                 '<a id="lightbox_close"></a>' +
-                '<a id="lightbox_left"><span id="lightbox_left_ico"></span></a>' +
-                '<a id="lightbox_right"><span id="lightbox_right_ico"></span></a>' +
+                '<a id="lightbox_left"><span class="lightbox_left_ico"></span></a>' +
+                '<a id="lightbox_right"><span class="lightbox_right_ico"></span></a>' +
             '</div>';
         wrap    = document.getElementById('lightbox_wrap');
         content = document.getElementById('lightbox_content');
         overlay = document.getElementById('lightbox_overlay');
+        $arrow_l = $('#lightbox_left');
+        $arrow_r = $('#lightbox_right');
     };
 
-    init();
-};
-$(GalleryOKFilm);
+    // Навешиваем обработчики клика на все фотки всех галерей на странице.
+    var _addEventListeners = function() {
+        var close_btn = document.getElementById('lightbox_close');
 
+            overlay.addEventListener('click', _closeLightBox);          // серая подложка закрывает просмотр
+          close_btn.addEventListener('click', _closeLightBox);          // крестик в углу закрывает просмотр
+        $arrow_l[0].addEventListener('click', _showPrevPhoto);          // стрелка влево на фото переключает на предыдущую фотку
+        $arrow_r[0].addEventListener('click', _showNextPhoto);          // стрелка вправо на фото переключает на следующую фотку
+               wrap.addEventListener('wheel', _changePhoto_wheel);      // вращение колёсика мышки на фотографии меняет фотографию
+           document.addEventListener('keydown', _changePhoto_keyboard); // смена фотографии клавиатурой
+        //swipe(document.getElementById('lightbox_wrap'), {             // смена фотографии и закрытие просмотра по свайпу
+        //    left:   function() { showNextPhoto(); },
+        //    right:  function() { showPrevPhoto(); },
+        //    top:    function() { closeLightBox(); },
+        //    bottom: function() { closeLightBox(); }
+        //});
+    };
 
+    // Открываем фотку в модальном окне
+    var _showOnePhoto = function(link) {
+        var winWidth  = window.innerWidth;          // ширина видимой области экрана
+        var winHeight = window.innerHeight;         // высота видимой области экрана
+        var scrollTop = $(window).scrollTop();      // верхняя часть видимой области окна
+        var scrollLeft = $(window).scrollLeft();    // левая часть видимой области окна
 
+        var img  = new Image;
 
+        img.src = link.href;
+        img.alt = link.title;
+        img.id  = 'lightbox_img';
+        img.onload = function() {
+            var ratio = img.naturalWidth / img.naturalHeight;
+            var H = winHeight * ratio * 0.9 <= winWidth ? winHeight * 0.9 : winWidth * 0.9 / ratio;
+            var W = (H - 20) * ratio + 20;
+            wrap.style.width  = W + 'px';
+            wrap.style.height = H + 'px';
+            wrap.style.top = (scrollTop + winHeight*0.05) + 'px';
+            wrap.style.left = (scrollLeft + (winWidth - W)/2) + 'px';
+            content.appendChild(img);
+            lightbox.style.display = "block";
+        };
 
-// Навешиваем обработчики клика на все фотки всех галерей на странице.
-$(function() {
-    document.getElementById('lightbox_overlay').addEventListener('click', closeLightBox);   // серая подложка закрывает просмотр
-    document.getElementById('lightbox_close').addEventListener('click', closeLightBox);     // крестик в углу закрывает просмотр
-    document.getElementById('lightbox_left').addEventListener('click', showPrevPhoto);      // стрелка влево на фото переключает на предыдущую фотку
-    document.getElementById('lightbox_right').addEventListener('click', showNextPhoto);     // стрелка вправо на фото переключает на следующую фотку
-    document.getElementById('lightbox_wrap').addEventListener('wheel', changePhoto_wheel);  // вращение колёсика мышки на фотографии меняет фотографию
-    document.addEventListener('keydown', changePhoto_keyboard);                             // смена фотографии клавиатурой
-    //swipe(document.getElementById('lightbox_wrap'), {                                       // смена фотографии и закрытие просмотра по свайпу
-    //    left:   function() { showNextPhoto(); },
-    //    right:  function() { showPrevPhoto(); },
-    //    top:    function() { closeLightBox(); },
-    //    bottom: function() { closeLightBox(); }
-    //});
+        link = $(link);
+        nextLink = link.parent().next('li').children()[0];       // сохраняем следующую за этой фотографию
+        prevLink = link.parent().prev('li').children()[0];       // сохраняем предыдущую перед этой фотографию
+        // Показать или скрыть стрелки переключения фотографий
+        if (nextLink) { $arrow_r.show(); } else { $arrow_r.hide(); }
+        if (prevLink) { $arrow_l.show(); } else { $arrow_l.hide(); }
+    };
 
-    var allGallerys = document.getElementsByClassName('gallery');
-    for (var i=0; i < allGallerys.length; i++) {
-        allGallerys[i].onclick = function(e) {
-            var target = e.target;
-            if(target.nodeName == 'IMG') {target = target.parentNode;}      // если нажатие пришлось не на ссылку а на вложенную в неё картинку, то получаем ссылку
-            // Обрабатываем все клики кроме клика колёсиком мыши.
-            // Потому что клик колёсиком должен просто открыть фотку в новой вкладке.
-            if (e.which != 2 && e.button != 1 && target.nodeName == 'A') {
-                showOnePhoto(target);
-                return false;
+    // Смена фотографии на следующую
+    var _showNextPhoto = function() {
+        if (nextLink) {
+            content.innerHTML = "";
+            _showOnePhoto(nextLink);
+        }
+    };
+
+    // Смена фотографии на предыдущую
+    var _showPrevPhoto = function() {
+        if (prevLink) {
+            content.innerHTML = "";
+            _showOnePhoto(prevLink);
+        }
+    };
+
+    // Смена фотографии с помощью стрелок на клавиатуре
+    var _changePhoto_keyboard = function(e) {
+        e = e || window.event;
+        if (lightbox.style.display == 'block') {
+            switch (e.keyCode) {
+                case 37: _showPrevPhoto(); e.preventDefault ? e.preventDefault() : (e.returnValue = false); break;
+                case 39: _showNextPhoto(); e.preventDefault ? e.preventDefault() : (e.returnValue = false); break;
             }
         }
-    }
-});
-
-
-// По клику на фотке открываем её в модальном окне
-function showOnePhoto(link) {
-    var winWidth  = window.innerWidth;          // ширина видимой области экрана
-    var winHeight = window.innerHeight;         // высота видимой области экрана
-    var scrollTop = $(window).scrollTop();      // верхняя часть видимой области окна
-    var scrollLeft = $(window).scrollLeft();    // левая часть видимой области окна
-
-    var img  = new Image;
-    var wrap = document.getElementById('lightbox_wrap');
-    var content = document.getElementById('lightbox_content');
-    var overlay = document.getElementById('lightbox_overlay');
-
-    img.src = link.href;
-    img.alt = link.title;
-    img.id  = 'lightbox_img';
-    img.onload = function() {
-        var ratio = img.naturalWidth / img.naturalHeight;
-        var H = winHeight * ratio * 0.9 <= winWidth ? winHeight * 0.9 : winWidth * 0.9 / ratio;
-        var W = (H - 20) * ratio + 20;
-        wrap.style.width  = W + 'px';
-        wrap.style.height = H + 'px';
-        wrap.style.top = (scrollTop + winHeight*0.05) + 'px';
-        wrap.style.left = (scrollLeft + (winWidth - W)/2) + 'px';
-        content.appendChild(img);
-        overlay.style.display = "block";
-        wrap.style.display = "block";
     };
 
-    link = $(link);
-    nextLink = link.parent().next('li').children()[0];       // сохраняем в глобальную переменную следующую за этой фотографию
-    prevLink = link.parent().prev('li').children()[0];       // сохраняем в глобальную переменную предыдущую перед этой фотографию
-    // Показать или скрыть стрелки переключения фотографий
-    if (nextLink) { $('#lightbox_right').show(); } else { $('#lightbox_right').hide(); }
-    if (prevLink) { $('#lightbox_left').show();  } else { $('#lightbox_left').hide(); }
-}
-// Смена фотографии на следующую
-function showNextPhoto() {
-    if (nextLink) {
-        document.getElementById('lightbox_content').innerHTML = "";
-        showOnePhoto(nextLink);
-    }
-}
-// Смена фотографии на предыдущую
-function showPrevPhoto() {
-    if (prevLink) {
-        document.getElementById('lightbox_content').innerHTML = "";
-        showOnePhoto(prevLink);
-    }
-}
+    // Смена фотографии с помощью колёсика мыши
+    var _changePhoto_wheel = function(e) {
+        e = e || window.event;
+        var delta = e.deltaY || e.detail || e.wheelDelta;   // величина поворота колеса мыши
+             if (delta > 0) { _showNextPhoto(); }           // колесо повернули к себе
+        else if (delta < 0) { _showPrevPhoto(); }           // колесо повернули от себя
+        e.preventDefault ? e.preventDefault() : (e.returnValue = false);    // отменяем стандартную прокрутку
+    };
 
-// Смена фотографии с помощью стрелок на клавиатуре
-function changePhoto_keyboard(e) {
-    e = e || window.event;
-    var lightBox = document.getElementById('lightbox_wrap');
-    if (lightBox.style.display == 'block') {
-        switch (e.keyCode) {
-            case 37: showPrevPhoto(); e.preventDefault ? e.preventDefault() : (e.returnValue = false); break;
-            case 39: showNextPhoto(); e.preventDefault ? e.preventDefault() : (e.returnValue = false); break;
-        }
-    }
-}
+    // Закрытие модального окна с фотографией
+    var _closeLightBox = function() {
+        nextLink = null;
+        prevLink = null;
+        lightbox.style.display = "none";
+        content.innerHTML = "";
+    };
 
-// Смена фотографии с помощью колёсика мыши
-function changePhoto_wheel(e) {
-    e = e || window.event;
-    var delta = e.deltaY || e.detail || e.wheelDelta;   // величина поворота колеса мыши
-    if (delta > 0) { showNextPhoto(); }                 // колесо повернули к себе
-    else if (delta < 0) { showPrevPhoto(); }            // колесо повернули от себя
-    e.preventDefault ? e.preventDefault() : (e.returnValue = false);    // отменяем стандартную прокрутку
-}
-
-// Закрытие модального окна с фотографией
-function closeLightBox() {
-    nextLink = undefined;
-    prevLink = undefined;
-    document.getElementById('lightbox_wrap').style.display = "none";
-    document.getElementById('lightbox_overlay').style.display = "none";
-    document.getElementById('lightbox_content').innerHTML = "";
-}
+    _init();
+};
+$(GalleryOKFilm);
